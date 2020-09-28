dc353a99df95a9bd19925a49e921b7989b522080
Takeru Hayasaka <hayatake396@gmail.com>
2020-09-28 13:31:36 +0900

    wip

--- cmd/create_map/main.go
+++ cmd/create_map/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"github.com/cilium/ebpf"
+	"fmt"
+)
 
 func main(){
-	fmt.Println("hello")
+	inner := &ebpf.MapSpec{
+		Type:       ebpf.Array,
+		KeySize:    4,
+		ValueSize:  4,
+		MaxEntries: 3,
+	}
+	outerArr := &ebpf.MapSpec{
+		Type:       ebpf.ArrayOfMaps,
+		KeySize:    4,
+		ValueSize:  4,
+		MaxEntries: 1,
+		InnerMap: inner,
+	}
+	outerHash := &ebpf.MapSpec{
+		Type:       ebpf.HashOfMaps,
+		KeySize:    4,
+		ValueSize:  4,
+		MaxEntries: 5,
+		InnerMap: inner,
+	}
+	fmt.Println(outerArr)
+	fmt.Println(outerHash)
+	
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/takehaya/test_ebpf_map
 
 go 1.14
+
+require github.com/cilium/ebpf v0.0.0-20200917151652-9b4cff7de01e
